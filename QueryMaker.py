52ad14fb85ecd2baeaa7df74c1965c12551ce86f
toadvine <drubbles@gmail.com>
2015-09-30 15:18:47 -0700

    putting logic and such into it

--- QueryMaker.py
+++ QueryMaker.py
@@ -1,1 +1,28 @@
 __author__ = 'benjamin_sanchez'
+
+
+def getList(i):
+    query_hold = list()
+    f = open(i, 'r')
+    for line in f:
+        line = line.translate('\n')
+        query_hold.append(line)
+    return query_hold
+
+
+def makeQuery(i):
+    answer = [int(x.rstrip('\n')) for x in i]
+    
+    return answer
+
+
+def writeOut(i):
+
+
+def main():
+    f = getList('inputList.txt')
+    a = makeQuery(f)
+    print (a)
+    #writeOut(a)
+
+main()
